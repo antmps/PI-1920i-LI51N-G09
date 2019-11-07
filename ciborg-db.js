6961bfe26cfe8a85cd66d8eb6e1774f3942b3711
antmps <37782476+antmps@users.noreply.github.com>
2019-11-07 18:50:09 +0000

    Altered module.exports

--- ciborg-db.js
+++ ciborg-db.js
@@ -3,47 +3,54 @@
 const request = require('request');
 //access to BD ElasticSearch
 
-const host = 'localhost:9200/';
-const baseUrl = `http://${host}`;
+module.exports = function(host) {
 
-function getGroups(cb){
-    const options = {
-        url: `${baseUrl}groups/_search`,
-        json: true
+    const baseUrl = `http://${host}`;
+
+    return {
+        getGroups : getGroups,
+        ciborgDbgetGroupsById : getGroupsById,
+        ciborgDbgetGroupGameByDuration : getGroupGameByDuration,
+        ciborgDbpostGroup : postGroup,
+        ciborgDbputGroupInfo : putGroupInfo,
+        ciborgDbputGameIntoGroup : putGameIntoGroup,
+        ciborgDbdeleteGameFromGroup : deleteGameFromGroup
     };
-    request.get
-    
+
+    function getGroups(cb) {
+        const options = {
+            url: `${baseUrl}groups/_search`,
+            json: true
+        };
+        request.get(options, (err,res,body)=>{
+            cb(err, body.hits.hits.map(e=>e._source));
+        })
+
+    }
+
+    function getGroupsById(groupId, cb) {
+
+    }
+
+    function getGroupGameByDuration(groupId, min, max, cb) {
+
+    }
+
+    function postGroup(groupName, description, cb) {
+
+    }
+
+    function putGroupInfo(groupId, groupName, description, cb) {
+
+    }
+
+    function putGameIntoGroup(groupId, gameId, cb) {
+
+    }
+
+    function deleteGameFromGroup(groupId, gameId, cb) {
+
+    }
+
+
 }
-
-function getGroupsById(groupId, cb){
-
-}
-
-function getGroupGameByDuration(groupId, min, max, cb){
-
-}
-
-function postGroup(groupName, description, cb){
-
-}
-
-function putGroupInfo(groupId, groupName, description, cb){
-
-}
-
-function putGameIntoGroup(groupId, gameId, cb){
-
-}
-
-function deleteGameFromGroup(groupId, gameId, cb){
-
-}
-
-
-module.exports.getGroups = getGroups;
-module.exports.getGroupsById = getGroupsById;
-module.exports.getGroupGameByDuration = getGroupGameByDuration;
-module.exports.postGroup = postGroup;
-module.exports.putGroupInfo = putGroupInfo;
-module.exports.putGameIntoGroup = putGameIntoGroup;
-module.exports.deleteGameFromGroup = deleteGameFromGroup;
