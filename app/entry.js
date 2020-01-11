23a2063bc056594579a15a9012a287456f7203ba
antmps <37782476+antmps@users.noreply.github.com>
2020-01-11 11:56:31 +0000

    aeawr

--- app/entry.js
+++ app/entry.js
@@ -1,10 +1,11 @@
 'use strict'
 
 require('../node_modules/bootstrap/dist/css/bootstrap.min.css')
+require('./assets/css/styles.css')
 require ('bootstrap')
 
 const templates = require('./templates')
-const bookshelfImg = require('./img/home_image.jpg')
+const bookshelfImg = require('./img/home_image_BW.jpg')
 
 window.addEventListener('hashchange', handler)
 handler()
@@ -21,6 +22,11 @@ function handler(){
             mainContent.innerHTML = templates.home({bookshelfImg})
             break;
         case 'games' :
+            mainContent.innerHTML = templates.games()
+            break;
+        case 'gamesearch' :
+            var gameName = document.getElementById('txt_Search_Games')
+            //service.getGameByName(gameName)
             break;
         default:
             window.location.hash="home"
--- app/templates.js
+++ app/templates.js
@@ -4,5 +4,7 @@ const Handlebars = require('../node_modules/handlebars/dist/handlebars')
 
 module.exports = {
     home: Handlebars.compile(require('./templates/home.hbs').default),
+    games: Handlebars.compile(require('./templates/games.hbs').default),
+    gamesearch : Handlebars.compile(require('./templates/gameTable.hbs').default),
     error: Handlebars.compile(require('./templates/error.hbs').default)
 } 
